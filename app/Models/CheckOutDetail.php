b7058e1f8b44bb541a80d37fcd58cf865accdbdd
uzzal71 <uzzalroy.acm@gmail.com>
2022-06-14 01:51:59 +0600

    check out details model and migration file update

--- app/Models/CheckOutDetail.php
+++ app/Models/CheckOutDetail.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class CheckOutDetail extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['check_out_id', 'item_id', 'quantity'];
 }
--- database/migrations/2022_06_13_093941_create_check_out_details_table.php
+++ database/migrations/2022_06_13_093941_create_check_out_details_table.php
@@ -15,7 +15,13 @@ return new class extends Migration
     {
         Schema::create('check_out_details', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('check_out_id');
+            $table->unsignedBigInteger('item_id');
+            $table->Integer('quantity');
             $table->timestamps();
+
+            $table->foreign('check_out_id')->references('id')->on('check_outs');
+            $table->foreign('item_id')->references('id')->on('items');
         });
     }
 
