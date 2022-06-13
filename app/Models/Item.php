ebfbcbfc6b40fbe7dde8555241f8cc450658457a
uzzal71 <uzzalroy.acm@gmail.com>
2022-06-14 00:07:22 +0600

    Item model, migration file update

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'item_name', 
+        'item_code', 
+        'item_barcode', 
+        'category_id', 
+        'brand_id', 
+        'item_quantity', 
+        'item_unit', 
+        'item_alert_quantity', 
+        'item_image', 
+    ];
 }
--- database/migrations/2022_06_13_092543_create_items_table.php
+++ database/migrations/2022_06_13_092543_create_items_table.php
@@ -15,7 +15,19 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->string('item_name');
+            $table->string('item_code');
+            $table->string('item_barcode');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('brand_id');
+            $table->intiger('item_quantity')->default(0);
+            $table->string('item_unit');
+            $table->intiger('item_alert_quantity')->default(5);
+            $table->string('item_image')->nullable();
             $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('brand_id')->references('id')->on('brands');
         });
     }
 
