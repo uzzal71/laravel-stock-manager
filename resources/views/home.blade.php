aa6dff43ce6a13277cafc6bb8b5aef5055f66ca7
uzzal71 <uzzalroy.acm@gmail.com>
2022-06-14 12:55:35 +0600

    Navbar bar and dashboard design completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,108 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="">
+        <h3>Welcome to Stock Manager</h3>
+        <p>Quick links and chart of for total number of check-ins and check-outs per month</p>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="content-panel">
+                <div class="table-responsive">
+                    <table class="table table-bordered dash">
+                        <tbody>
+                            <tr>
+                                <td class="text-center btn-theme03">
+                                    <a href="#"><i class="fa fa-barcode"></i> <span>Items</span></a>
+                                </td>
+                                <td class="text-center btn-theme03">
+                                    <a href="#"><i class="fa fa-arrow-circle-up"></i> <span>Check-in Items</span></a>
+                                </td>
+                                <td class="text-center btn-theme03">
+                                    <a href="#"><i class="fa fa-arrow-circle-down"></i> <span>Check-out Items</span></a>
+                                </td>
+                                <td class="text-center btn-theme03">
+                                    <a href="#"><i class="fa fa-users"></i> <span>Users</span></a>
+                                </td>
+                                <td class="text-center btn-theme03">
+                                    <a href="#"><i class="fa fa-cogs"></i> <span>Settings</span></a>
+                                </td>
+                                <td class="text-center btn-theme03">
+                                    <a href="#"><i class="fa fa-download"></i> <span>Backups</span></a>
+                                </td>
+                                <td class="text-center btn-theme03">
+                                    <a href="#"><i class="fa fa-upload"></i> <span>Updates</span></a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<style type="text/css">
+    .content-panel {
+        padding-top: 15px;
+        margin-bottom: 40px;
+    }
+
+    .dash tr {
+        background-color: #48cfad;
+    }
+
+    .dash td {
+        width: 10%;
+        padding: 0!important;
+    }
+
+    .dash td a {
+        color: #FFF;
+        display: block;
+        padding: 15px 5px;
+    }
+
+    a, a:focus, a:hover {
+        text-decoration: none;
+        outline: 0;
+    }
+
+    .dash i {
+        font-size: 4em;
+        margin-bottom: 10px;
+    }
+
+    .fa {
+        display: inline-block;
+        font-family: FontAwesome;
+        font-weight: 400;
+        line-height: 1;
+    }
+
+    .dash span {
+        display: block;
+    }
+    .bold, .dash span {
+        font-weight: 700;
+    }
+
+    .btn-theme03 {
+        color: #fff;
+        background-color: #48cfad;
+        border-color: #37bc9b;
+    }
+    .text-center {
+        text-align: center;
+    }
+
+    .btn-theme03.active, .btn-theme03:active, .btn-theme03:focus, .btn-theme03:hover, .open .dropdown-toggle.btn-theme03 {
+        color: #fff;
+        background-color: #37bc9b;
+        border-color: #37bc9b;
+    }
+
+    .table-bordered, .table-bordered>tbody>tr>td, .table-bordered>tbody>tr>th, .table-bordered>tfoot>tr>td, .table-bordered>tfoot>tr>th, .table-bordered>thead>tr>td, .table-bordered>thead>tr>th {
+    border: 1px solid #ddd;
+}
+</style>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,13 +18,38 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <style type="text/css">
+        ul.top-menu>li>a {
+            color: #666;
+            font-size: 12px;
+            border-radius: 4px;
+            -webkit-border-radius: 4px;
+            border: 1px solid #666!important;
+            padding: 5px 10px;
+            margin-right: 10px;
+            font-weight: bold;
+        }
+
+        ul.top-menu>li>.logout {
+            color: #f2f2f2;
+            font-size: 12px;
+            border-radius: 4px;
+            -webkit-border-radius: 4px;
+            border: 1px solid #64c3c2!important;
+            padding: 5px 10px;
+            background: #68dff0;
+        }
+    </style>
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" style="background: #ffd777 !important;border-bottom: 1px solid #c9aa5f;">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'Stock Manager') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -37,7 +62,7 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
+                    <ul class="navbar-nav ms-auto top-menu">
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
@@ -54,7 +79,128 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                    <i class="fa fa-barcode"></i>
+                                    <span class="hidden-xs hidden-sm">Items</span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">
+                                        Settings
+                                    </a>
+
+                                    <a class="dropdown-item" href="#">
+                                        Categories
+                                    </a>
+
+                                    <a class="dropdown-item" href="#">
+                                        Add Category
+                                    </a>
+
+                                    <a class="dropdown-item" href="#">
+                                        Backups
+                                    </a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    <i class="fa fa-arrow-circle-up"></i>
+                                    <span class="hidden-xs hidden-sm">Check-Ins</span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">
+                                        Settings
+                                    </a>
+
+                                    <a class="dropdown-item" href="#">
+                                        Categories
+                                    </a>
+
+                                    <a class="dropdown-item" href="#">
+                                        Add Category
+                                    </a>
+
+                                    <a class="dropdown-item" href="#">
+                                        Backups
+                                    </a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    <i class="fa fa-arrow-circle-down"></i>
+                                    <span class="hidden-xs hidden-sm">Check-Outs</span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">
+                                        Settings
+                                    </a>
+
+                                    <a class="dropdown-item" href="#">
+                                        Categories
+                                    </a>
+
+                                    <a class="dropdown-item" href="#">
+                                        Add Category
+                                    </a>
+
+                                    <a class="dropdown-item" href="#">
+                                        Backups
+                                    </a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    <i class="fa fa-users"></i>
+                                    <span class="hidden-xs hidden-sm">People</span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">
+                                        Settings
+                                    </a>
+
+                                    <a class="dropdown-item" href="#">
+                                        Categories
+                                    </a>
+
+                                    <a class="dropdown-item" href="#">
+                                        Add Category
+                                    </a>
+
+                                    <a class="dropdown-item" href="#">
+                                        Backups
+                                    </a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    <i class="fa fa-cogs"></i>
+                                    <span class="hidden-xs hidden-sm">Settings</span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">
+                                        Settings
+                                    </a>
+
+                                    <a class="dropdown-item" href="#">
+                                        Categories
+                                    </a>
+
+                                    <a class="dropdown-item" href="#">
+                                        Add Category
+                                    </a>
+
+                                    <a class="dropdown-item" href="#">
+                                        Backups
+                                    </a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle logout" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    <i class="fa fa-user"></i>
+                                    <span class="hidden-xs hidden-sm capitalize">Hi! {{ Auth::user()->name }}</span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
