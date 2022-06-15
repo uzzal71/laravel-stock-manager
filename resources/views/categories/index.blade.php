2a904e63be67fcabc233748292af2b1fc7e68f68
uzzal71 <uzzalroy.acm@gmail.com>
2022-06-16 00:11:10 +0600

    Category index page updated

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -5,11 +5,11 @@
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Supplier Management</h2>
+            <h2>Category Management</h2>
         </div>
         <div class="pull-right">
-            @can('suppliers.create')
-            <a class="btn btn-success" href="{{ route('suppliers.create') }}"> Create New Supplier </a>
+            @can('categories.create')
+            <a class="btn btn-success" href="{{ route('categories.create') }}"> Create New Category </a>
             @endcan
         </div>
     </div>
@@ -26,28 +26,26 @@
 <table class="table table-bordered">
  <tr>
    <th>No</th>
-   <th>Name</th>
-   <th>Email</th>
-   <th>Phone</th>
+   <th>Category Code</th>
+   <th>Category Name</th>
    <th width="280px">Action</th>
  </tr>
- @foreach ($data as $key => $supplier)
+ @foreach ($data as $key => $category)
   <tr>
     <td>{{ ++$i }}</td>
-    <td>{{ $supplier->supplier_name }}</td>
-    <td>{{ $supplier->supplier_email }}</td>
-    <td>{{ $supplier->supplier_phone }}</td>
+    <td>{{ $category->category_code }}</td>
+    <td>{{ $category->category_name }}</td>
     <td>
-        @can('suppliers.show')
-       <a class="btn btn-info" href="{{ route('suppliers.show',$supplier->id) }}">Show</a>
+        @can('categories.show')
+       <a class="btn btn-info" href="{{ route('categories.show',$category->id) }}">Show</a>
        @endcan
 
-       @can('suppliers.edit')
-       <a class="btn btn-primary" href="{{ route('suppliers.edit',$supplier->id) }}">Edit</a>
+       @can('categories.edit')
+       <a class="btn btn-primary" href="{{ route('categories.edit',$category->id) }}">Edit</a>
        @endcan
 
-       @can('suppliers.delete')
-        {!! Form::open(['method' => 'DELETE','route' => ['suppliers.destroy', $supplier->id],'style'=>'display:inline']) !!}
+       @can('categories.delete')
+        {!! Form::open(['method' => 'DELETE','route' => ['categories.destroy', $category->id],'style'=>'display:inline']) !!}
             {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
         {!! Form::close() !!}
         @endcan
