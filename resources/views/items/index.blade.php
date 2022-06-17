66906b78f0e38d3a48f51e71d9d5e2f55871fa59
uzzal71 <uzzalroy.acm@gmail.com>
2022-06-17 15:24:15 +0600

    ItemController Update

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -5,11 +5,11 @@
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Customer Management</h2>
+            <h2>Item Management</h2>
         </div>
         <div class="pull-right">
-            @can('customers.create')
-            <a class="btn btn-success" href="{{ route('customers.create') }}"> Create New Customer </a>
+            @can('items.create')
+            <a class="btn btn-success" href="{{ route('items.create') }}"> Create New Item </a>
             @endcan
         </div>
     </div>
@@ -27,27 +27,27 @@
  <tr>
    <th>No</th>
    <th>Name</th>
-   <th>Email</th>
-   <th>Phone</th>
+   <th>Code</th>
+   <th>Barcode</th>
    <th width="280px">Action</th>
  </tr>
- @foreach ($data as $key => $customer)
+ @foreach ($data as $key => $item)
   <tr>
     <td>{{ ++$i }}</td>
-    <td>{{ $customer->customer_name }}</td>
-    <td>{{ $customer->customer_email }}</td>
-    <td>{{ $customer->customer_phone }}</td>
+    <td>{{ $item->items_name }}</td>
+    <td>{{ $item->item_code }}</td>
+    <td>{{ $item->item_barcode }}</td>
     <td>
-        @can('customers.show')
-       <a class="btn btn-info" href="{{ route('customers.show',$customer->id) }}">Show</a>
+        @can('items.show')
+       <a class="btn btn-info" href="{{ route('items.show',$item->id) }}">Show</a>
        @endcan
 
-       @can('customers.edit')
-       <a class="btn btn-primary" href="{{ route('customers.edit',$customer->id) }}">Edit</a>
+       @can('items.edit')
+       <a class="btn btn-primary" href="{{ route('items.edit',$item->id) }}">Edit</a>
        @endcan
 
-       @can('customers.delete')
-        {!! Form::open(['method' => 'DELETE','route' => ['customers.destroy', $customer->id],'style'=>'display:inline']) !!}
+       @can('items.delete')
+        {!! Form::open(['method' => 'DELETE','route' => ['items.destroy', $item->id],'style'=>'display:inline']) !!}
             {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
         {!! Form::close() !!}
         @endcan
