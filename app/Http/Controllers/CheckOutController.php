7b4639c1b82b16bffb92651c97817e8ec25704c7
uzzal71 <uzzalroy.acm@gmail.com>
2022-06-20 23:50:38 +0600

    CheckOutController Import Route File

--- app/Http/Controllers/CheckOutController.php
+++ app/Http/Controllers/CheckOutController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class CheckOutController extends Controller
-{
+{ 
     /**
      * Display a listing of the resource.
      *
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\CheckInController;
+use App\Http\Controllers\CheckOutController;
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RoleController;
