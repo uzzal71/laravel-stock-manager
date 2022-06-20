d52e8f3cbecbd999c5758e20a9213a5208dfbee8
uzzal71 <uzzalroy.acm@gmail.com>
2022-06-20 23:51:30 +0600

    CheckOutController Define Route

--- routes/web.php
+++ routes/web.php
@@ -50,4 +50,5 @@ Route::group(['middleware' => ['auth']], function() {
   Route::resource('barcodes', BarcodeController::class);
   Route::resource('items', ItemController::class);
   Route::resource('checkin', CheckInController::class);
+  Route::resource('checkin', CheckOutController::class);
 });
