5e3029da912e27bd960977014eb58acc1494b447
uzzal71 <uzzalroy.acm@gmail.com>
2022-06-23 00:59:07 +0600

    Checkout create page updated

--- resources/views/checkout/create.blade.php
+++ resources/views/checkout/create.blade.php
@@ -5,10 +5,10 @@
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Create New Item</h2>
+            <h2>Create New Checkin</h2>
         </div>
         <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('items.index') }}"> Back </a>
+            <a class="btn btn-primary" href="{{ route('checkin.index') }}"> Back </a>
         </div>
     </div>
 </div>
@@ -25,62 +25,73 @@
   </div>
 @endif
 
+<div class="row mt-2">
+    <div class="col-xs-6 col-sm-6 col-md-6">
+        <div class="form-group">
+            <strong>Date</strong>
+            {!! Form::date('item_name', null, array('placeholder' => 'Item Name','class' => 'form-control')) !!}
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-6 col-md-6">
+        <div class="form-group">
+            <strong>Reference:</strong>
+            {!! Form::text('reference', null, array('placeholder' => 'Reference','class' => 'form-control')) !!}
+        </div>
+    </div>
+</div>
 
+<div class="row mt-2">
+    <div class="col-xs-6 col-sm-6 col-md-6">
+        <div class="form-group">
+            <strong>Supplier</strong>
+            <select class="form-control" name="supplier_id">
+                <option value="1">Select Supplier</option>
+                @foreach($suppliers as $supplier)
+                <option value="{{ $supplier->id }}">{{ $supplier->supplier_name }}({{ $supplier->id }})</option>
+                @endforeach
+            </select>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-6 col-md-6">
+        <div class="form-group">
+            <strong>Add Item:</strong>
+            <select class="form-control" name="item_id">
+            <option value="1">Select Item</option>
+                @foreach($items as $item)
+                <option value="{{ $supplier->id }}">{{ $item->item_name }}({{ $item->item_code }})</option>
+                @endforeach
+            </select>
+        </div>
+    </div>
+</div>
 
-{!! Form::open(array('route' => 'items.store','method'=>'POST')) !!}
-<div class="row">
+<div class="row mt-4">
     <div class="col-xs-12 col-sm-12 col-md-12">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Quantity</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</div>
+
+<div class="row mt-2">
+<div class="col-xs-12 col-sm-12 col-md-12">
         <div class="form-group">
-            <strong>Item Name:</strong>
-            {!! Form::text('item_name', null, array('placeholder' => 'Item Name','class' => 'form-control')) !!}
+            <strong>Note:</strong>
+            <textarea class="form-control" name="note"></textarea>
         </div>
     </div>
+</div>
+
+<div class="row mt-2">
     <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Item Code:</strong>
-            {!! Form::text('item_code', null, array('placeholder' => 'Item Code','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Barcode:</strong>
-            {!! Form::select('item_barcode', ['Code25' => 'Code25', 'Code39' => 'Code39', 'Code128' => 'Code128', 'EAN8' => 'EAN8', 'EAN13' => 'EAN13', 'UPC-A' => 'UPC-A', 'UPC-E' => 'UPC-E'], null, ['placeholder' => 'Pick a barcode', 'class' => 'form-control']) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Category:</strong>
-            {!! Form::select('category_id', ['1' => 'Jamel Gibson', '2' => 'Miss Augusta Schinner', '3' => 'Gregoria Howell', '4' => 'Melissa Hayes', '5' => 'Mrs. Juana Crona', '6' => 'Chyna Herzog', '7' => 'Gillian Kutch', '8' => 'Prof. Josh Pfeffer I', '9' => 'Neil Sanford', '10' => 'Dr. Novella Pagac'], null, ['placeholder' => 'Pick a Category', 'class' => 'form-control']) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-    <div class="form-group">
-            <strong>Brand:</strong>
-            {!! Form::select('brand_id', ['1' => 'Trudie Turner', '2' => 'Jena Jaskolski', '3' => 'Dominic Kerluke III', '4' => 'Scot Bartoletti', '5' => 'Prof. Darron Hintz', '6' => 'Adam Runte', '7' => 'Ari Jacobs', '8' => 'Aiyana Pouros', '9' => 'Jacklyn Wolf', '10' => 'Brad Barton'], null, ['placeholder' => 'Pick a Brand', 'class' => 'form-control']) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Quantity:</strong>
-            {!! Form::text('item_quantity', null, array('placeholder' => 'Quantity','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Unit:</strong>
-            {!! Form::text('item_unit', null, array('placeholder' => 'Unit','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-        <strong>Alert Quantity:</strong>
-            {!! Form::text('item_alert_quantity', null, array('placeholder' => 'Alert Quantity','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
         <button type="submit" class="btn btn-primary">Submit</button>
     </div>
 </div>
-{!! Form::close() !!}
 
 @endsection
