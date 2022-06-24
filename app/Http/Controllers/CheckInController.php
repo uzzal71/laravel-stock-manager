b6e24a058055c8c66b2ae78a6a9e47f06857202f
uzzal71 <uzzalroy.acm@gmail.com>
2022-06-24 22:34:56 +0600

    CheckInController CheckIn Model Save Code Write

--- app/Http/Controllers/CheckInController.php
+++ app/Http/Controllers/CheckInController.php
@@ -58,6 +58,12 @@ class CheckInController extends Controller
     public function store(Request $request)
     {
         $checkin = new CheckIn;
+        $checkin->checkin_date = $request->checkin_date;
+        $checkin->reference = $request->reference;
+        $checkin->supplier_id = $request->supplier_id;
+        $checkin->note = $request->note;
+
+        $checkin->save();
     }
 
     /**
